71bf1d6c2bcb6f690c67e5d3ab83d4ba7de75a3c
amaniCoding <amanuelfrm@gmail.com>
2025-01-04 16:48:25 +0300

    updated

--- app/ui/login.tsx
+++ app/ui/login.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Login() {
   return (
@@ -25,8 +26,8 @@ export default function Login() {
             <input type="text" className="py-3 px-4 block w-full border-2 border-gray/45 rounded-md" placeholder="Enter your email or username"></input>
             <input type="password" className="py-3 px-4 my-5 block w-full border-2 border-gray/45 rounded-md" placeholder="Enter your email or username"></input>
             <div className="flex md:flex-row flex-col md:items-center md:justify-between mb-5">
-              <button type="submit" className="p-2 px-4 bg-blue-500 text-center text-white rounded-md block md:w-24 md:mb-0 mb-5 w-full ">Login</button>
-              <button type="submit" className="p-2 border-2 border-gray-800/35 text-center text-black rounded-md block md:w-24 w-full ">Reginster</button>
+              <Link href={`/home`} type="submit" className="p-2 px-4 bg-blue-500 text-center text-white rounded-md block md:w-24 md:mb-0 mb-5 w-full ">Login</Link>
+              <Link href={`/home`} type="submit" className="p-2 border-2 border-gray-800/35 text-center text-black rounded-md block md:w-24 w-full ">Reginster</Link>
             </div>
             <div className="text-sm">All rights reserved 2025</div>
           </div>
